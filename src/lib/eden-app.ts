82f70ef47b262138c0d55b368a15d9ae17882f97
ripabak <faruqirifa2@gmail.com>
2026-05-03 15:46:27 +0700

    fix: eden treaty app to support creds

--- src/lib/eden-app.ts
+++ src/lib/eden-app.ts
@@ -1,5 +1,32 @@
-// client.ts
-import type { App } from '@/src/types/app'
-import { treaty } from '@elysia/eden'
+// lib/eden-app.ts
+import type { App } from "@/src/types/app.d.ts";
+import { treaty } from "@elysia/eden";
+import { authClient } from "./auth-client";
 
-export const app = treaty<App>("https://cukkr-backend-dev.fire.my.id")
+export const app = treaty<App>(
+  process.env.EXPO_PUBLIC_ENV_API_URL!,
+  {
+    onRequest: async (path, options) => {
+      const cookies = authClient.getCookie();
+
+      return {
+        ...options,
+        headers: {
+          ...(options?.headers || {}),
+          ...(cookies ? { Cookie: cookies } : {}),
+        },
+      };
+    },
+
+    onResponse: async (response) => {
+      if (response.status === 401) {
+        console.error("Unauthorized request", { status: response.status });
+      }
+      return response;
+    },
+
+    fetch: {
+      credentials: "include"
+    },
+  }
+);
